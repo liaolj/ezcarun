e933f044d5936ef70889cb8710c849c2dfd7e910
liaolijiang <liaolj@ihep.ac.cn>
2017-12-14 17:58:52 +0800

    finish

--- main.go
+++ main.go
@@ -10,19 +10,22 @@ func main() {
 	// d := ebridge.Edemo()
 	value1, err1 := ebridge.LongGet("ll")
 	if err1 != nil {
-		fmt.Printf("[longin]-ll is %v\n", value1)
 	}
+	fmt.Printf("[longin]-ll is %v\n", value1)
 
 	value2, err2 := ebridge.StringGet("str")
 	if err2 != nil {
-		fmt.Printf("[stringin]-str is %v\n", value2)
 	}
-	value3, err3 := ebridge.DoubleGet("dd")
+	fmt.Printf("[stringin]-str is %v\n", value2)
+
+	value3, err3 := ebridge.DoubleGet("aa")
 	if err3 != nil {
-		fmt.Printf("[ai]-dd is %v\n", value3)
 	}
+	fmt.Printf("[ai]-dd is %v\n", value3)
+
 	value4, err4 := ebridge.BoolGet("bb")
 	if err4 != nil {
-		fmt.Printf("[bi]-bb is %v\n", value4)
 	}
+	fmt.Printf("[bi]-bb is %v\n", value4)
+
 }
