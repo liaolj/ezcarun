6c3a840cae071b67f5fe2fb9ec8701d209cad245
liaolijiang <liaolj@ihep.ac.cn>
2017-12-14 17:53:37 +0800

    run

--- main.go
+++ main.go
@@ -8,8 +8,21 @@ import (
 
 func main() {
 	// d := ebridge.Edemo()
-	value, err := ebridge.LongGet("epicsHost:ai1")
+	value, err := ebridge.LongGet("ll")
 	if err != nil {
-		fmt.Printf("result is %d\n", value)
+		fmt.Printf("[longin]-ll is %v\n", value)
+	}
+
+	value, err := ebridge.StringGet("str")
+	if err != nil {
+		fmt.Printf("[stringin]-str is %v\n", value)
+	}
+	value, err := ebridge.DoubleGet("dd")
+	if err != nil {
+		fmt.Printf("[ai]-dd is %v\n", value)
+	}
+	value, err := ebridge.BoolGet("bb")
+	if err != nil {
+		fmt.Printf("[bi]-bb is %v\n", value)
 	}
 }
